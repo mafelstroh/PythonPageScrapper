2fe1ca8fc693c628cf3a9bc7da8c2518127e2c8f
Manuel F. Stroh S <donmafelstroh@gmail.com>
2016-11-17 17:04:35 -0500

    Finishing HTML parser class before implementation

--- classes/HtmlPageParser.py
+++ classes/HtmlPageParser.py
@@ -2,7 +2,4 @@ import HTMLParser
 
 class HtmlPageParser(HTMLParser.HTMLParser):
     def handle_starttag(self, tag, attrs):
-        print "Encountered a start tag:", tag
-
-    def handle_endtag(self, tag):
-        print "Encountered an end tag :", tag
+        tag
